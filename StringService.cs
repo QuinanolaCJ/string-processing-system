2996ba3760e4a77860c50dfa4c5349168c8a75ab
Quiñanola CJ <quinanolacj303@gmail.com>
2026-02-28 10:45:14 +0800

    Implement StringService with text manipulation methods

---
+++ StringService.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace StringProcessing.Services
+{
+    public class StringService
+    {
+        private string currentText = "";
+        private string originalText = "";
+
+        public void SetText(string text)
+        {
+            currentText = text;
+            originalText = text;
+        }
+
+        public string GetText()
+        {
+            return currentText;
+        }
+
+        public void ToUpperCase()
+        {
+            currentText = currentText.ToUpper();
+        }
+
+        public void ToLowerCase()
+        {
+            currentText = currentText.ToLower();
+        }
+
+        public int CountCharacters()
+        {
+            return currentText.Length;
+        }
+
+        public bool ContainsWord(string word)
+        {
+            return currentText.Contains(word);
+        }
+
+        public void ReplaceWord(string oldWord, string newWord)
+        {
+            currentText = currentText.Replace(oldWord, newWord);
+        }
+
+        public void ExtractSubstring(int startIndex, int length)
+        {
+            currentText = currentText.Substring(startIndex, length);
+        }
+
+        public void TrimSpaces()
+        {
+            currentText = currentText.Trim();
+        }
+
+        public void ResetText()
+        {
+            currentText = originalText;
+        }
+    }
+}
