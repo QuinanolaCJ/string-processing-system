602052410f3af6ab7f6c884897ec389340d320d9
Quiñanola CJ <quinanolacj303@gmail.com>
2026-02-28 10:52:57 +0800

    Add Program.cs with main entry point

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using StringProcessing.Views;
+
+namespace StringProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringView view = new StringView();
+            view.Run();
+        }
+    }
+}
